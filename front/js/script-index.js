66fcb5925e8aadfa4252da89bb7fd6ab31d168d6
Sandra TESSIER <madame.sandra.tessier@gmail.com>
2022-03-03 16:20:16 +0100

    modifications script index (ok)+ produit(=erreur)

--- front/js/script-index.js
+++ front/js/script-index.js
@@ -28,16 +28,17 @@ fetch("http://localhost:3000/api/products")
         let image = document.createElement("img");
         image.src = canape.imageUrl;
         image.alt = canape.altTxt;
-        article.appendChild(image);
-
+        
         let titreH3 = document.createElement("h3");
         titreH3.classList.add("productName");
         titreH3.innerText = canape.name;
-        article.appendChild(titreH3);
-
+        
         let description = document.createElement("p");
         description.classList.add("productionDescription");
         description.innerText = canape.description;
+        
+        article.appendChild(image);
+        article.appendChild(titreH3);
         article.appendChild(description);
 
         baliseA.appendChild(article);        
@@ -45,23 +46,4 @@ fetch("http://localhost:3000/api/products")
         .appendChild(baliseA); 
     }
 
-    //methode 1
-//mettre un énévement
-const clickVignette=document.querySelector("a");
-clickVignette.addEventListener("click", function(){
-    var adresseWeb = "http://localhost:3000/api/products/http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926";
-    var url = new URL(adresseWeb);
-    var id = url.searchParams.get("_id");
-    console.log(id);
-})  
-
-
-/*methode 2
-var adresseWeb = "https://waytolearnx.com/t.html?name=alex-babtise&age=25&address=paris";
-var url = new URL(adresseWeb);
-var search_params = new URLSearchParams(url.search); 
-if(search_params.has('name')) {
-  var name = search_params.get('name');
-  console.log(name)
-}*/
 
--- front/js/script-product.js
+++ front/js/script-product.js
@@ -1,17 +1,47 @@
-/*Appeler l'API 
-fetch("http://localhost:3000/api/products")
+//Récuperer l'id de l'url du produit avec *?*
+const urlIdProduit = window.location.search;
+
+//Extraire l'id numérique (sans *?*)
+const urlIdNumerique = new URLSearchParams(urlIdProduit);
+const IdNumerique = urlIdNumerique.get("id");
+//console.log(IdNumerique);
+
+//Appeler l'API 
+fetch("http://localhost:3000/api/products/"+IdNumerique)
     .then(function(reponse){
         if(reponse.ok) {
             return reponse.json();
         }
     })
-    .then(function(canapes){
-        //console.table(canapes);
-        for (const elementCanape of canapes) {
-            ajouterCanape(elementCanape);            
-        }
+    .then(function(canapeProduit){
+        //console.table(canapeProduit);      
+        recupererInformationsDuCanape(canapeProduit);            
     })
     .catch(function(erreur){
         alert("une erreur est survenue");
-    });*/
+    });
+
+/**
+ * fonction pour recupérer et afficher les données d'un article dans la page Produit
+ * @param {*} unCanape 
+ */
+
+function recupererInformationsDuCanape (unCanape){
+
+        let image = document.createElement("img");
+        image.src = unCanape.imageUrl;
+        image.alt = unCanape.altTxt;
+        let divImg = document.getElementsByClassName("item__img");
+        divImg.appendChild(image);        
+
+        let titreH1 = document.getElementById("title");
+        titreH1.innerText = unCanape.name;
+
+        let prixCanape = document.getElementById("price");
+        prixCanape.innerText = unCanape.price;
+
+        let optionCouleur = document.createElement("option");
+        optionCouleur.name = unCanape.colors;
+};
+
 
